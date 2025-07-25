ed149fa453ad04597af26d378aea0ae9d22e07bb
marcelo-egoeimi <marcelo@egoeimi.dev>
2025-07-25 10:11:08 -0300

    feat(index): organize type exports in index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export * from './onboarding';
+export * from './location';
+export * from './quorum';
+export * from './notification';
+export * from './filter';
+export * from './swipe';
---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export * from './colors';
+export * from './archetypes';
+export * from './thresholds';
+export * from './events';
+
+// import { COMPATIBILITY_THRESHOLDS, JOYN_COLORS, PERSONALITY_ARCHETYPES, EVENT_CATEGORIES } from '../constants';
